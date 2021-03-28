87b3a0f8bcd7405827cd95ee7954938c4282b0d1
Mike Lin <dna@mlin.net>
2021-03-27 18:47:19 -1000

    in the beginning

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # GenomicSQLite-examples
+
+This repo exhibits a small example program using [Genomics Extension for SQLite](https://github.com/mlin/GenomicSQLite), implemented in several supported programming languages. It's meant as a minimal starting point for you to fork into your own program. See the [Programming Guide](https://mlin.github.io/GenomicSQLite) for full documentation.
+
+The program `gsqlex load exons.gff exons.db` imports a small [GFF file](https://en.wikipedia.org/wiki/General_feature_format) into a database, indexed by genomic range. Then, `gsqlex query exons.db chr17:43,104,800-43,105,000` prints the records overlapping the given genomic range.
+
+All of the following example invocations begin from the root directory of this repository.
+
+### Python
+
+```
+$ pip install genomicsqlite
+$ python/gsqlex.py load exons.gff exons.db
+$ python/gsqlex.py query exons.db chr17:43,104,800-43,105,000
+```
---
+++ python/gsqlex.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import sys
+import sqlite3
+import genomicsqlite
+
+
+def load(input_gff, output_db):
+    # create compressed database file and schema therein
+    dbconn: sqlite3.Connection = genomicsqlite.connect(output_db, unsafe_load=True)
+    dbconn.executescript(
+        """CREATE TABLE gff(
+            chromosome TEXT COLLATE UINT,
+            begin_pos INTEGER,
+            end_pos INTEGER,
+            line TEXT)"""
+    )
+
+    # fill in the GFF lines
+    count = 0
+    with open(input_gff) as infile:
+        for line in infile:
+            line = line.strip()
+            fields = line.split("\t")
+            chromosome = fields[0]
+            begin_pos = int(fields[1]) - 1
+            end_pos = int(fields[2])
+            dbconn.execute(
+                "INSERT INTO gff(chromosome,begin_pos,end_pos,line) VALUES(?,?,?,?)",
+                (chromosome, begin_pos, end_pos, line),
+            )
+            count += 1
+    print(f"Loaded {count} GFF lines")
+
+    # create Genomic Range Index (GRI) on the GFF records
+    create_gri_sql = genomicsqlite.create_genomic_range_index_sql(
+        dbconn, "gff", "chromosome", "begin_pos", "end_pos"
+    )
+    dbconn.executescript(create_gri_sql)
+
+    # COMMIT
+    dbconn.commit()
+
+
+def query(db, range_txt):
+    # open compressed database for reading
+    dbconn: sqlite3.Connection = genomicsqlite.connect(db, read_only=True)
+
+    # query the genomic range using in-SQL helper functions for GRI and parsing the range string
+    cursor = dbconn.execute(
+        """SELECT line FROM gff WHERE _rowid_ IN
+                genomic_range_rowids(
+                    'gff',
+                    parse_genomic_range_sequence(?1),
+                    parse_genomic_range_begin(?1),
+                    parse_genomic_range_end(?1))""",
+        (range_txt,),
+    )
+
+    # print results
+    for row in cursor:
+        print(row[0])
+
+
+def main(argv):
+    if argv[1] == "load":
+        load(argv[2], argv[3])
+    elif argv[1] == "query":
+        query(argv[2], argv[3])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
